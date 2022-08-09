5852bea53245ff022c98b3f2e4023c064bb71166
Ketanpatel0306 <Ketan_patel@codenticsoftware.com>
2022-08-09 09:28:59 +0530

    styled component add

--- pages/new-data.js
+++ pages/new-data.js
@@ -1,10 +1,36 @@
 import React from "react";
-import { Header } from "../componant";
-
+import styled from "styled-components";
 export default function NewData() {
+  const StyledComponent = styled.div`
+    display: -webkit-flex;
+    -webkit-flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    color: black;
+    font-size: 1em;
+    margin: 1em;
+    padding: 0.25em 1em;
+    border: 5px solid yellow;
+    border-radius: 3px;
+    background-color: red;
+  `;
+  const Button = styled.button`
+    background-color: yellow;
+    border: none;
+    color: green;
+    cursor: pointer;
+  `;
+  const Tomato = styled(StyledComponent)`
+    background-color: pink;
+    cursor: pointer;
+  `;
   return (
     <div>
-      <h2>hello NewData</h2>
+      <StyledComponent>
+        <h2>this is new-data page</h2>
+        <Button>Click me</Button>
+        <Tomato>Ignore me</Tomato>
+      </StyledComponent>
     </div>
   );
 }
